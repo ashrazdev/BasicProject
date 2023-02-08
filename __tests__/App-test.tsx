73982ac6f47ee695753f066bb5e30ff700f83099
ashraz <ashraz.developer@gmail.com>
2023-02-08 13:34:30 +0500

    add jest

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,11 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('Renders Message', async () => {
+  const {toJSON} = render(<App />);
+
+  expect(toJSON()).toMatchSnapshot();
 });
